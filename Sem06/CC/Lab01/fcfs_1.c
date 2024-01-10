6cc989ab5edc7f8b22d319a8856ac9d3d427b141
Raj Aryan <anuragraj718@gmail.com>
2024-01-10 14:40:58 +0530

    sem06

---
+++ Sem06/CC/Lab01/fcfs_1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+struct fcfs
+{
+    int pid;
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
+    int start;
+    int end;
+};
+void line(int x)
+{
+    for (int i = 0; i < x; i++)
+    {
+        printf("-");
+    }
+    printf("\n");
+}
+
+void main()
+{
+    int num;
+    float turnavg = 0.0, turnsum = 0.0, waitsum = 0.0;
+    printf("Enter the total number of processes: ");
+    scanf("%d", &num);
+
+    struct fcfs f[20], temp;
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("Enter arrival time of process %d: ", i + 1);
+        scanf("%d", &f[i].arrival);
+        printf("Enter burst time of process %d: ", i + 1);
+        scanf("%d", &f[i].burst);
+        f[i].pid = i + 1;
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = i + 1; j < num; j++)
+        {
+            if (f[j].arrival < f[i].arrival)
+            {
+                temp = f[j];
+                f[j] = f[i];
+                f[i] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        f[i].start = turnsum > f[i].arrival ? turnsum : f[i].arrival;
+        turnsum = f[i].start + f[i].burst;
+        f[i].end = turnsum;
+        f[i].turnaround = f[i].end - f[i].arrival;
+        f[i].waiting = f[i].turnaround - f[i].burst;
+    }
+
+    turnsum = 0;
+    line(70);
+    printf("PID\tArrival\tBurst\tStart\tEnd\tWaiting\tTurnaround\n");
+    line(70);
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", f[i].pid, f[i].arrival, f[i].burst, f[i].start, f[i].end, f[i].waiting, f[i].turnaround);
+        turnsum += f[i].turnaround;
+        waitsum += f[i].waiting;
+    }
+    line(70);
+    turnavg = turnsum / (float)num;
+    printf("\nAverage Turnaround time: %.2f.", turnavg);
+    printf("\nAverage Waiting time: %.2f\n", waitsum / 5.0);
+}
+
