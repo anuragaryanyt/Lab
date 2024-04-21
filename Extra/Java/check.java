6c1c2683ef392c373c4e89854484590038ec0e2c
anuragaryanyt <anuragraj718@gmail.com>
2024-04-21 15:22:48 +0530

    changes

--- Extra/Java/check.java
+++ Extra/Java/check.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class TwoDArrays {
+public class check {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter no of rows: ");
